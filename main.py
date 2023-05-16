0a22df35208fcedc15ed14e8975e3aa03cc46ba6
Elkin Kirill <74110907+kirillv11@users.noreply.github.com>
2023-05-16 21:11:59 +0300

    Update main.py
    added comments and optimized work

--- main.py
+++ main.py
@@ -1,86 +1,100 @@
-from random import randint
 import json
+import wikipedia
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
-import wikipedia
+from random import choice
 from config import API_TOKEN
 
-
-# t.me/ExplorationUniverseBot
+# Initialize wikipedia language
+wikipedia.set_lang('ru')
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
 
+
+# Load data from JSON file
+file = open('data.json', encoding='utf-8')
+templates = json.load(file)
+
+
+# This function is called when user sends '/start' command
 @dp.message_handler(commands=['start'])
 async def start_command(message: Message):
-    """
-    This function will be called when user sends `/start` command
-    """
-    await message.answer("Здравствуйте! С помощью данного бота вы сможете изучить космос и увидеть уникальные фотографии. Весь список команд можно узнать в меню команд, около ввода сообщения, или ввести команду /help.")
+	"""
+	Sends greeting message to the user
+	"""
+	await message.answer(
+		"Здравствуйте! С помощью данного бота вы сможете изучить космос и увидеть уникальные фотографии. Весь список команд можно узнать в меню команд, около ввода сообщения, или ввести команду /help."
+		)
 
 
+# This function returns help text to the user
 @dp.message_handler(commands=['help'])
 async def help_command(message: Message):
-    """
-    This function return hepl text
-    """
-    await message.answer("/start - запуск\n/foto - случайное фото\n/fact - случайный факт\n/person - случайный человек, который связан с исследованием вселенной\nзапрос без /, например 'солнце', - вернёт ответ на запрос")
+	"""
+	Sends list of available commands to the user
+	"""
+	await message.answer(
+		"/start - запуск\n/foto - случайное фото\n/fact - случайный факт\n/person - случайный человек, который связан с исследованием вселенной\nзапрос без /, например 'солнце', - вернёт ответ на запрос"
+		)
 
 
+# This function returns a random space image to the user
 @dp.message_handler(commands=['foto', 'get_foto', 'image'])
 async def space_image(message: Message):
-    """
-    This function retur random space image
-    """
-    with open('data.json', encoding='utf-8') as file:
-        file_content = file.read()
-        templates = json.loads(file_content)
-        length = len(templates['image'])
-        number = randint(0, length - 1)
-        with open(templates['image'][number]['path'], "rb") as photo:
-            await message.answer_photo(photo, caption=f"<b>{templates['image'][number]['name']}</b>\n\n{templates['image'][number]['description']}")
+	"""
+	This function return random space image
+	"""
+	image = choice(templates['image'])
+	with open(image['path'], "rb") as photo:
+		await message.answer_photo(
+			photo,
+			caption=f"<b>{image['name']}</b>\n\n{image['description']}"
+		)
 
-    
+
+# This function returns a random fact about space to the user
 @dp.message_handler(commands=['fact', 'get_foto'])
 async def space_fact(message: Message):
-    """
-    This function return random fact about space
-    """
-    with open('data.json', encoding='utf-8') as file:
-        file_content = file.read()
-        templates = json.loads(file_content)
-        length = len(templates['facts'])
-        number = randint(0, length - 1)
-        await message.answer(f"<b>Интересный факт</b>\n\n{templates['facts'][number]}")
+	"""
+	This function return random fact about space
+	"""
+	fact = choice(templates['facts'])
+	await message.answer(f"<b>Интересный факт</b>\n\n{fact}")
 
 
+# This function returns information about a person associated with space exploration to the user
 @dp.message_handler(commands=['person'])
 async def person(message: Message):
-    """
-    This function return person Which is associated with space
-    """
-    with open('data.json', encoding='utf-8') as file:
-        file_content = file.read()
-        templates = json.loads(file_content)
-        length = len(templates['person'])
-        number = randint(0, length - 1)
-        with open(templates['person'][number]['path_image'], "rb") as photo:
-            await message.answer_photo(photo, caption=f"<b>{templates['person'][number]['fullname']}</b>\n\nГоды жизни: {templates['person'][number]['date']}\n\n<a href=\"{templates['person'][number]['url']}\">читать подробнее</a>")
+	"""
+	This function return person Which is associated with space
+	"""
+	data_person = choice(templates['person'])
+	with open(data_person['path_image'], "rb") as photo:
+		await message.answer_photo(
+			photo,
+			caption=f"<b>{data_person['fullname']}</b>\n\nГоды жизни: {data_person['date']}\n\n<a href=\"{data_person['url']}\">читать подробнее</a>"
+		)
 
 
+# This function searches for a Wikipedia article based on the user's input
 @dp.message_handler()
 async def search_wikipedia(message: Message):
-    """
-    This function return wikipedia article
-    """
-    try:
-        wikipedia.set_lang('ru')
-        response = wikipedia.page(message.text)
-        await message.answer(f"<b>{response.title}</b>\n\n{response.summary}\n\n<a href=\"{response.url}\">читать подробнее</a>")
-    except:
-        await message.answer(f"Ошибка! Ответ на запрос '{message.text}' не найден.")
+	"""
+	This function return wikipedia article
+	"""
+	try:
+		response = wikipedia.page(message.text)
+		await message.answer(
+			f"<b>{response.title}</b>\n\n{response.summary}\n\n<a href=\"{response.url}\">читать подробнее</a>"
+			)
+	except wikipedia.exceptions.PageError:
+		await message.answer(f"Ошибка! Страница не найдена.")
+	except:
+		await message.answer(f"Ошибка! Ответ на запрос '{message.text}' не найден.")
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+	executor.start_polling(dp, skip_updates=True)
+	file.close()
