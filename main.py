fa81016be01e62c4ec7208d0b5ee00c5c2c85f07
Elkin Kirill <74110907+kirillv11@users.noreply.github.com>
2024-03-29 23:26:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import json
 import wikipedia
+from random import choice
+
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import Message
-from random import choice
+
 from config import API_TOKEN
+
+
+# --- Constants and Initialization ---
 
 # Initialize wikipedia language
 wikipedia.set_lang('ru')
@@ -14,16 +19,18 @@ dp = Dispatcher(bot)
 
 
 # Load data from JSON file
-file = open('data.json', encoding='utf-8')
-templates = json.load(file)
+def load_data(filename):
+    with open(filename, encoding="utf-8") as file:
+        return json.load(file)
+
+
+templates = load_data("data.json")
 
 
 # This function is called when user sends '/start' command
 @dp.message_handler(commands=['start'])
 async def start_command(message: Message):
-	"""
-	Sends greeting message to the user
-	"""
+	"""Sends a greeting message to the user."""
 	await message.answer(
 		"Здравствуйте! С помощью данного бота вы сможете изучить космос и увидеть уникальные фотографии. Весь список команд можно узнать в меню команд, около ввода сообщения, или ввести команду /help."
 		)
@@ -32,9 +39,7 @@ async def start_command(message: Message):
 # This function returns help text to the user
 @dp.message_handler(commands=['help'])
 async def help_command(message: Message):
-	"""
-	Sends list of available commands to the user
-	"""
+	"""Sends a list of available commands to the user."""
 	await message.answer(
 		"/start - запуск\n/foto - случайное фото\n/fact - случайный факт\n/person - случайный человек, который связан с исследованием вселенной\nзапрос без /, например 'солнце', - вернёт ответ на запрос"
 		)
@@ -43,9 +48,7 @@ async def help_command(message: Message):
 # This function returns a random space image to the user
 @dp.message_handler(commands=['foto', 'get_foto', 'image'])
 async def space_image(message: Message):
-	"""
-	This function return random space image
-	"""
+	"""Sends a random space image to the user."""
 	image = choice(templates['image'])
 	with open(image['path'], "rb") as photo:
 		await message.answer_photo(
@@ -57,9 +60,7 @@ async def space_image(message: Message):
 # This function returns a random fact about space to the user
 @dp.message_handler(commands=['fact', 'get_foto'])
 async def space_fact(message: Message):
-	"""
-	This function return random fact about space
-	"""
+	"""Sends a random space fact to the user."""
 	fact = choice(templates['facts'])
 	await message.answer(f"<b>Интересный факт</b>\n\n{fact}")
 
@@ -67,9 +68,7 @@ async def space_fact(message: Message):
 # This function returns information about a person associated with space exploration to the user
 @dp.message_handler(commands=['person'])
 async def person(message: Message):
-	"""
-	This function return person Which is associated with space
-	"""
+	"""Sends information about a random person associated with space exploration."""
 	data_person = choice(templates['person'])
 	with open(data_person['path_image'], "rb") as photo:
 		await message.answer_photo(
@@ -78,12 +77,9 @@ async def person(message: Message):
 		)
 
 
-# This function searches for a Wikipedia article based on the user's input
 @dp.message_handler()
 async def search_wikipedia(message: Message):
-	"""
-	This function return wikipedia article
-	"""
+	"""Searches Wikipedia for the user's query and sends the result."""
 	try:
 		response = wikipedia.page(message.text)
 		await message.answer(
@@ -91,8 +87,10 @@ async def search_wikipedia(message: Message):
 			)
 	except wikipedia.exceptions.PageError:
 		await message.answer(f"Ошибка! Страница не найдена.")
-	except:
-		await message.answer(f"Ошибка! Ответ на запрос '{message.text}' не найден.")
+	except Exception:
+		await message.answer(
+			f"Ошибка! Ответ на запрос '{message.text}' не найден."
+		)
 
 
 if __name__ == '__main__':
